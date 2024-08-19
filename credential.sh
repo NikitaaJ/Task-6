4f8d95a2733b673011b9021cf971ef07a459b865
Nikita Jethani <107095302+NikitaaJ@users.noreply.github.com>
2024-08-19 15:50:16 +0530

    Update credential.sh

--- credential.sh
+++ credential.sh
@@ -2,8 +2,8 @@ export VAULT_ADDR='http://192.168.1.85:8200'
 export VAULT_TOKEN='root'
  
 # Fetch credentials from Vault and store them in variables
-EMAIL=$(vault kv get -field=email secret/screener)
-PASSWORD=$(vault kv get -field=password secret/screener)
+EMAIL=$(vault kv get -field=email secret/data/screener)
+PASSWORD=$(vault kv get -field=password secret/data/screener)
  
 # Save credentials to files
 echo "$EMAIL" > credentials/email.txt
