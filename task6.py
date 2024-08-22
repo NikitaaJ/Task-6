4e52aa48b61849c9cfb490e6b5704521cd3fa13a
Nikita Jethani <107095302+NikitaaJ@users.noreply.github.com>
2024-08-22 14:02:04 +0530

    Update task6.py

--- task6.py
+++ task6.py
@@ -51,7 +51,7 @@ def scrape_reliance_data(session):
         df = pd.DataFrame(row_data, columns=headers)
         df = df.drop('TTM', axis=1)  # Drop the TTM column
         df_transposed = df.transpose().reset_index()
-        df_transposed.rename(columns={'index': 'Narration'}, inplace=True)
+        df_transposed.rename(columns={'index': 'year'}, inplace=True)
         df_transposed = df_transposed.reset_index(drop=True)
         df_transposed = df_transposed.replace('', 0)  # Replace empty strings with 0
         df_transposed = df_transposed.replace(np.nan, 0)  # Replace null values with 0
